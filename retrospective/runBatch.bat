bb6a1e58653d6e9db0180b60832c9d41242fb0cb
Martin Schlegel <m.schlegel81@googlemail.com>
2019-05-04 09:05:50 +0200

    Initial commit

---
+++ retrospective/runBatch.bat
@@ -0,0 +1,14 @@
+runWithAll.mnh x\hello.mnh 7
+runWithAll.mnh x\test_each.mnh 5
+runWithAll.mnh x\test_peach.mnh 5
+runWithAll.mnh x\test_map.mnh 5
+runWithAll.mnh x\test_pmap.mnh 5
+runWithAll.mnh x\test_filter.mnh 5
+runWithAll.mnh x\test_pfilter.mnh 5
+runWithAll.mnh x\100doors.mnh 7
+runWithAll.mnh x\99bottles.mnh 5
+runWithAll.mnh x\ackermann.mnh 5
+runWithAll.mnh x\monteCarloPi.mnh 7
+runWithAll.mnh x\sumTo100.mnh 5
+runWithAll.mnh x\vampire.mnh 3
+trimLog.mnh

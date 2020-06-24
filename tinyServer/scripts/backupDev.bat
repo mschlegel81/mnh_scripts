4d3738acb11a8ccda5becc61b76b05dc08d9d550
Martin Schlegel <m.schlegel81@googlemail.com>
2020-06-24 23:32:03 +0200

    Tiny server

--- tinyServer/scripts/backupDev.bat
+++ tinyServer/scripts/backupDev.bat
@@ -1,6 +1,5 @@
 chcp 65001
 D:
 cd dev
-pushtoUsb.mnh zip
+mnh smart7z.mnh
 robocopy D:\dev D:\backup\dev *.7z /MIR /NP /LEV:1 /FFT /COPY:DT
-mnh -cmd "folders('*.*').pMap({print($x,' ',$x.allFiles('*.*').sort.fileStats.serialize.sha256)})"
--- tinyServer/scripts/sync.bat
+++ tinyServer/scripts/sync.bat
@@ -2,7 +2,7 @@ chcp 65001
 rem robocopy "N:\Martin\TmForever\Tracks\Challenges\My Challenges" "C:\users\schlegel\Documents\TmForever\Tracks\Challenges\My Challenges" /FFT /S
 
 rem robocopy N:\dev D:\dev *.7z /MIR /NP /LEV:1 /FFT /COPY:DT
-robocopy N:\dev\art.bin D:\dev\art.bin /MIR /COPY:DT
+rem robocopy N:\dev\art.bin D:\dev\art.bin /MIR /COPY:DT
 rem robocopy N:\dev\mnh5 D:\dev\mnh5 /S /NP /FFT /COPY:DT /XO
 rem robocopy N:\dev\retrospective D:\dev\retrospective /S /NP /FFT /COPY:DT /XO
 rem robocopy N:\dev\retrospective D:\dev\retrospective /MIR /COPY:DT
@@ -13,15 +13,10 @@ rem robocopy D:\dokumente\Sylwia N:\Sylwia /FFT /S
 rem robocopy D:\dokumente\Martin N:\Martin /FFT /S
 rem robocopy D:\dokumente\Oskar N:\Oskar /FFT /S
 
-rem robocopy N:\Sylwia D:\dokumente\Sylwia /FFT /S
-rem robocopy N:\Martin D:\dokumente\Martin /FFT /S
-rem robocopy N:\Oskar D:\dokumente\Oskar /FFT /S
+robocopy N:\Sylwia D:\dokumente\Sylwia /FFT /S
+robocopy N:\Martin D:\dokumente\Martin /FFT /S
+robocopy N:\Oskar D:\dokumente\Oskar /FFT /S
 
 D:
 cd dev 
 incrementalBackup.mnh
-cd mnh5
-make.mnh status
-demos\inputs\inputs.mnh
-mnh -v1io -cmd "httpGet('http://192.168.1.100:80/addTask?scriptName=backupDev%2Ebat&cmdLineParameters=')"
-mnh -v1io -cmd "httpGet('http://192.168.1.100/addTask?scriptName=buildMnh%2Ebat&cmdLineParameters=status%20build%20status%20test%20status')"
